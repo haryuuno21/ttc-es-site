e095696f81ef8a758fd579340038dbcfa582da6f
haryuuno21 <82949896+haryuuno21@users.noreply.github.com>
2025-07-10 13:54:04 +0300

    fixed module bug

--- ttc-es/src/app/contact/page.tsx
+++ ttc-es/src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 import { FeedbackForm } from "@/components/feedbackForm";
-import styles from "./page.module.css";
+import styles from "./contact_page.module.css";
 import { Navbar } from "@/components/navbar";
 
 export default function Contact() {
@@ -7,7 +7,7 @@ export default function Contact() {
     <div className={styles.page}>
       <Navbar></Navbar>
       <main className={styles.main}>
-        <div>
+        <div className={styles.feedbackBlock}>
           <h1>Обратная связь</h1>
           <FeedbackForm></FeedbackForm>
         </div>
@@ -15,12 +15,36 @@ export default function Contact() {
           <h1>Контакты</h1>
           <article className={styles.article}>
             <h2>Офис "ТВЕРСКАЯ"</h2>
-            <p>Адрес:</p>
-            <p>125047, г. Москва, 1-я Тверская-Ямская ул., д. 23, корп. 1, офис 705.</p>
-            <p>Телефон:</p>
-            <p>+7 (495) 789 20 90</p>
-            <p>Почта:</p>
-            <p>info@ttc-es.com</p>
+            <div>
+              <p>Адрес:</p>
+              <p>
+                125047, г. Москва, 1-я Тверская-Ямская ул., д. 23, корп. 1, офис
+                705.
+              </p>
+            </div>
+            <div>
+              <p>Телефон:</p>
+              <p>+7 (495) 789 20 90</p>
+            </div>
+            <div>
+              <p>Почта:</p>
+              <p>info@ttc-es.com</p>
+            </div>
+          </article>
+          <article className={styles.article}>
+            <h2>Производственное подразделение</h2>
+            <div>
+              <p>Адрес:</p>
+              <p>105486, Россия, г. Москва, 16-я Парковая ул., д. 26, корп.1</p>
+            </div>
+            <div>
+              <p>Телефон:</p>
+              <p>+7 (495) 789 20 90</p>
+            </div>
+            <div>
+              <p>Почта:</p>
+              <p>info@ttc-es.com</p>
+            </div>
           </article>
         </div>
       </main>
