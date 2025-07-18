c2c9e545f3ffc4e2ad39da353a8d13c8fdbadcb3
haryuuno21 <82949896+haryuuno21@users.noreply.github.com>
2025-07-18 19:17:36 +0300

    product card

--- ttc-es/src/app/contact/page.tsx
+++ ttc-es/src/app/contact/page.tsx
@@ -1,53 +1,13 @@
 import { FeedbackForm } from "@/components/feedbackForm";
 import styles from "./contact_page.module.css";
 import { Navbar } from "@/components/navbar";
+import { CatalogCard } from "@/components/catalogCard";
 
 export default function Contact() {
   return (
     <div className={styles.page}>
-      <Navbar></Navbar>
-      <main className={styles.main}>
-        <div className={styles.feedbackBlock}>
-          <h1>Обратная связь</h1>
-          <FeedbackForm></FeedbackForm>
-        </div>
-        <div className={styles.contacts}>
-          <h1>Контакты</h1>
-          <article className={styles.article}>
-            <h2>Офис "ТВЕРСКАЯ"</h2>
-            <div>
-              <p>Адрес:</p>
-              <p>
-                125047, г. Москва, 1-я Тверская-Ямская ул., д. 23, корп. 1, офис
-                705.
-              </p>
-            </div>
-            <div>
-              <p>Телефон:</p>
-              <p>+7 (495) 789 20 90</p>
-            </div>
-            <div>
-              <p>Почта:</p>
-              <p>info@ttc-es.com</p>
-            </div>
-          </article>
-          <article className={styles.article}>
-            <h2>Производственное подразделение</h2>
-            <div>
-              <p>Адрес:</p>
-              <p>105486, Россия, г. Москва, 16-я Парковая ул., д. 26, корп.1</p>
-            </div>
-            <div>
-              <p>Телефон:</p>
-              <p>+7 (495) 789 20 90</p>
-            </div>
-            <div>
-              <p>Почта:</p>
-              <p>info@ttc-es.com</p>
-            </div>
-          </article>
-        </div>
-      </main>
+      
+      <CatalogCard/>
     </div>
   );
 }
---
+++ ttc-es/src/components/catalogCard/ui/index.types.ts
@@ -0,0 +1,6 @@
+export interface Props {
+    img?: string;
+    product_name?: string;
+    product_short_name?: string;
+    product_cost?: string;
+}
