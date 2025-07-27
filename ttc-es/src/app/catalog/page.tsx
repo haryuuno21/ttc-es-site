04ea5fd36a14ea81414d11f2166682bb9f3f22ff
agentbonjovi <149247113+agentbonjovi@users.noreply.github.com>
2025-07-27 22:52:12 +0300

    idk
    aergh

--- ttc-es/src/app/catalog/page.tsx
+++ ttc-es/src/app/catalog/page.tsx
@@ -5,12 +5,12 @@ import { Navbar } from "@/components/navbar";
 export default function Contact() {
   // Пример данных для карточек (можно заменить на реальные данные)
   const products = [
-    { id: 1, img: "/product.png", product_name: "Продукт 1", product_short_name: "PCM30U-1", product_cost: 10000 },
-    { id: 2, img: "/product.png", product_name: "Продукт 2", product_short_name: "PCM30U-2", product_cost: 15000 },
-    { id: 3, img: "/product.png", product_name: "Продукт 3", product_short_name: "PCM30U-3", product_cost: 20000 },
-    { id: 4, img: "/product.png", product_name: "Продукт 4", product_short_name: "PCM30U-4", product_cost: 25000 },
-    { id: 5, img: "/product.png", product_name: "Продукт 5", product_short_name: "PCM30U-5", product_cost: 30000 },
-    { id: 6, img: "/product.png", product_name: "Продукт 6", product_short_name: "PCM30U-6", product_cost: 35000 },
+    { id: 1, img: "/product.png", product_name: "Продукт 1", product_short_name: "PCM30U-1", product_cost: '10000' },
+    { id: 2, img: "/product.png", product_name: "Продукт 2", product_short_name: "PCM30U-2", product_cost: '15000' },
+    { id: 3, img: "/product.png", product_name: "Прукощпиукргшпиujrgbhuiedrbgvetighaeuiogvhni1290378423784652347856", product_short_name: "PCM30U-3", product_cost: '20000' },
+    { id: 4, img: "/product.png", product_name: "Продукт 4", product_short_name: "PCM30U-4", product_cost: '25000' },
+    { id: 5, img: "/product.png", product_name: "Продукт 5", product_short_name: "PCM30U-5", product_cost: '30000' },
+    { id: 6, img: "/product.png", product_name: "Продукт 6", product_short_name: "PCM30U-6", product_cost: '35000' },
     // Добавьте больше продуктов по необходимости
   ];
 
